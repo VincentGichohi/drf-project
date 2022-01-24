d702a4730dd734a9a4e066edd35d4de73c5d0e85
VincentGichohi <vincentegichohi@gmail.com>
2022-01-24 10:52:43 +0300

    First Changes

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
