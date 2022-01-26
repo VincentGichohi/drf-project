fed28cfdce4b843dbbf1e319d80aaf1a2f64d2f8
VincentGichohi <vincentegichohi@gmail.com>
2022-01-26 11:57:02 +0300

    added

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from watchlist_app.api.views import movie_list, movie_details
+# from watchlist_app.api.views import movie_list, movie_details
+from watchlist_app.api.views import WatchListAV, WatchListDetailAV, StreamPlatFormListAV, StreamPlatFormDetailAV
 # from . import views
+
 urlpatterns = [
-    path('list/', movie_list, name='movie-list,'),
-    path('<int:pk>/', movie_details, name='movie-detail')
+    path('list/', WatchListAV.as_view(), name='movie-list,'),
+    path('<int:pk>/', WatchListDetailAV.as_view(), name='movie-detail'),
+    path('stream/', StreamPlatFormListAV.as_view(), name='stream'),
+    path('stream/<int:pk>/', StreamPlatFormDetailAV.as_view(), name='stream-detail')
 ]
